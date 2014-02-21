38638d32bbf4371c7d0f7ecaf9e09ab33bb8ce07
Tim Kranen <tim.kranen@skindustries.net>
2014-02-21 10:50:37 +0100

    Added spaceclient

---
+++ SpaceClient/src/spaceclient/Direction.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spaceclient;
+
+/**
+ *
+ * @author Tim
+ */
+public enum Direction {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT,
+    NEUTRAL;
+}
