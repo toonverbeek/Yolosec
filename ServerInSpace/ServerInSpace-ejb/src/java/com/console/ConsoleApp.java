53dba8658c64cb96a2be728cceb9a81ef7402445
KILL-MementoMori <p.f.sanderse@student.fontys.nl>
2014-02-28 10:35:26 +0100

    database user identification method

---
+++ ServerInSpace/ServerInSpace-ejb/src/java/com/console/ConsoleApp.java
@@ -0,0 +1,40 @@
+package com.console;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ConsoleApp {
+    
+    private final Boolean isRunning;
+    public ConsoleApp() {
+        System.out.println("Starting console...");
+        System.out.println("Available commands are:");
+        System.out.println("-getStatus");
+        System.out.println("-exit");
+        isRunning = true;
+        init();
+    }
+    
+    private void init(){
+        String input = "";
+        while(isRunning) {
+            Scanner sc = new Scanner(System.in);
+            input = sc.next();
+            switch(input) {
+                case "getStatus":
+                    System.out.println("((getstatus))");
+                    break;
+                case "exit":
+                    //Runtime.getRuntime().exit(0);
+                    System.exit(0);
+                default:
+                    System.out.println("Command not recognized.");
+                    break;
+            }
+        }
+    }
+    
+}
