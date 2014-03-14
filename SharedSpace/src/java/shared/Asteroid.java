9503fd3261a28651795f73fc4e79fbcd98fb33b8
sgijsber <bolletje94@gmail.com>
2014-03-14 14:11:21 +0100

    Shared resource

---
+++ SharedSpace/src/java/shared/Asteroid.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package shared;
+
+/**
+ *
+ * @author user
+ */
+public class Asteroid {
+    
+    private AsteroidType type;
+    private int resourceAmount;
+    private float x,y;
+
+    public Asteroid(AsteroidType type, int resourceAmount, float x, float y) {
+        this.type = type;
+        this.resourceAmount = resourceAmount;
+        this.x = x;
+        this.y = y;
+    }
+
+    public AsteroidType getType() {
+        return type;
+    }
+
+    public int getResourceAmount() {
+        return resourceAmount;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }   
+}
