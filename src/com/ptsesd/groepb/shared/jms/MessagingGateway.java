9b60751b4661a1d66cf32b85e9c2e4f67c5a86da
Tim Kranen <tim.kranen@skindustries.net>
2014-04-03 12:14:00 +0200

    MessagingGateway finished

--- src/com/ptsesd/groepb/shared/jms/MessagingGateway.java
+++ src/com/ptsesd/groepb/shared/jms/MessagingGateway.java
@@ -3,27 +3,98 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.ptsesd.groepb.shared.jms;
 
+import com.ptsesd.groepb.shared.ItemComm;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /**
  *
  * @author Tim
  */
 public abstract class MessagingGateway {
-    
-    private static String factoryName = "connectionFactory";
-    
+
+    private Connection connection;
+    protected Session session;
+    private MessageProducer mProducer;
+    private MessageConsumer mConsumer;
+    private static String factoryName = "queueConnectionFactory";
+    private Context jndiContext;
+    private String requestQueue;
+    private String replyQueue;
+
     public MessagingGateway(String requestQueue, String replyQueue) {
-        
+        this.requestQueue = requestQueue;
+        this.replyQueue = replyQueue;
+        setupConnection();
     }
-    
+
+    private void setupConnection() {
+        try {
+            jndiContext = new InitialContext();
+            ConnectionFactory factory = (ConnectionFactory) jndiContext.lookup(factoryName);
+            connection = factory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            setDestinations();
+        } catch (NamingException | JMSException ex) {
+            Logger.getLogger(MessagingGateway.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void setDestinations() {
+        try {
+            Destination sendToDestination = (Destination) jndiContext.lookup(requestQueue);
+            mProducer = session.createProducer(sendToDestination);
+
+            Destination replyFromDestination = (Destination) jndiContext.lookup(replyQueue);
+            mConsumer = session.createConsumer(replyFromDestination);
+            mConsumer.setMessageListener(new MessageListener() {
+
+                @Override
+                public void onMessage(Message message) {
+                    onReceivedMessage(message);
+                }
+            });
+        } catch (JMSException | NamingException ex) {
+            Logger.getLogger(MessagingGateway.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     public abstract void onReceivedMessage(Message msg);
-    
-    private void sendMessage() {
-        
+
+    public void sendItemComm(ItemComm item) {
+        Message msg = null;
+        ItemSerializer serializer = new ItemSerializer();
+        String json = serializer.itemToJson(item);
+        try {
+            msg = session.createTextMessage(json);
+            this.mProducer.send(msg);
+        } catch (JMSException ex) {
+            Logger.getLogger(MessagingGateway.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-     
+
+    public void openConnection() {
+        try {
+            if (connection != null) {
+                connection.start();
+            }
+        } catch (JMSException ex) {
+            Logger.getLogger(MessagingGateway.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
