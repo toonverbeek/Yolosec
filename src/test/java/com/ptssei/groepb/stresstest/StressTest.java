07e3f85ccc1f5d9ccf3feb14bc3893ff9c17306f
KILL-MementoMori <p.f.sanderse@student.fontys.nl>
2014-04-15 15:41:33 +0200

    cpu logging feature added

--- src/test/java/com/ptssei/groepb/stresstest/StressTest.java
+++ src/test/java/com/ptssei/groepb/stresstest/StressTest.java
@@ -1,18 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.ptssei.groepb.stresstest;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import com.yolosec.util.ConnectionString;
 import java.net.SocketException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -21,103 +18,238 @@ import org.junit.Test;
  * @author user
  */
 public class StressTest extends TestCase {
-    
-    private JFrame guiFrame;
-    
+
     private ExecutorService executor;
-    private final int amountOfClients = 5;
-    
+    private final int amountOfClients = 10;
+    private int amountLoggedIn = 0;
+    private static final int USERIDBASE = 2000000000;
+    private static final int WAITTIME = 5000;
+//    private static final String IP_ADDRESS = "127.0.0.1";
+//    private static final String IP_ADDRESS = "192.168.24.78‏";
+    private static final String IP_ADDRESS = "145.93.57.150";
+    private static HashMap<Integer, String> userList;
+    private static boolean hasData = false;
+    private static int testsCompleted = 0;
+    private static final int numberOfTests = 1;
+
     private boolean running = true;
-    
+
     public StressTest(String testName) {
         super(testName);
     }
-    
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         //Instantiate variables
-        executor = Executors.newFixedThreadPool(amountOfClients);
-        
-        //Set up GUI
-        guiFrame = new JFrame();
-        guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        guiFrame.setTitle("Example GUI");
-        guiFrame.setSize(800,600);
-        JButton newButton = new JButton("Close");
-        newButton.setSize(100, 50);
-        newButton.addActionListener(new ActionListener() {
+        if (!hasData) {
+            executor = Executors.newFixedThreadPool(amountOfClients);
 
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                System.out.println("---[STRESSTEST] Clicked");
-                System.out.println("---[STRESSTEST] Running [" + running +"]");
-                running = false;
-                System.out.println("---[STRESSTEST] Running [" + running +"]");
+            //Add users to the database
+            userList = new HashMap<>();
+            Exception ErrM = null;
+            Connection connect = null;
+            PreparedStatement preparedStatementSpaceship = null;
+            PreparedStatement preparedStatementUser = null;
+            try {
+                System.out.println(String.format("---[DATABASE] %s", ConnectionString.getConnectionString()));
+                System.out.println("---[DATABASE] Setting up connection...");
+                Class.forName("com.mysql.jdbc.Driver");
+                connect = DriverManager.getConnection(ConnectionString.getConnectionString());
+                System.out.println("---[DATABASE] Connection established");
+                //clear database of old testdata
+                preparedStatementUser = connect.prepareStatement("DELETE FROM account WHERE spaceship_id >= ?;");
+                preparedStatementSpaceship = connect.prepareStatement("DELETE FROM spaceship WHERE id >= ?;");
+                preparedStatementUser.setInt(1, USERIDBASE);
+                preparedStatementSpaceship.setInt(1, USERIDBASE);
+                preparedStatementUser.executeUpdate();
+                preparedStatementSpaceship.executeUpdate();
+                //end 
+                String username;
+                Integer spaceshipid;
+                Random r = new Random();
+                preparedStatementSpaceship = connect.prepareStatement("INSERT INTO spaceship (id, position_x, position_y) VALUES (?, ?, ?);");
+                preparedStatementUser = connect.prepareStatement("INSERT INTO account (username, password, spaceship_id) VALUES (?, ?, ?);");
+                for (int i = 0; i < amountOfClients; i++) {
+                    username = "testuser" + i;
+                    spaceshipid = USERIDBASE + i;
+
+                    preparedStatementSpaceship.setInt(1, spaceshipid);
+                    preparedStatementSpaceship.setInt(2, r.nextInt(65535));
+                    preparedStatementSpaceship.setInt(3, r.nextInt(65535));
+                    preparedStatementUser.setString(1, username);
+                    preparedStatementUser.setString(2, username);
+                    preparedStatementUser.setInt(3, spaceshipid);
+
+                    preparedStatementSpaceship.executeUpdate();
+                    preparedStatementUser.executeUpdate();
+                    userList.put(spaceshipid, username);
+                }
+            } catch (ClassNotFoundException | SQLException e) {
+                ErrM = e;
+            } finally {
+                preparedStatementUser.close();
+                preparedStatementSpaceship.close();
+                connect.close();
             }
-        });
-        
-        guiFrame.add(newButton);
-        
+            //check if an error was thrown while creating the users
+            System.out.println("---" + amountOfClients + " Users created---");
+            if (ErrM != null) {
+                System.out.println("Errors occurred: " + ErrM.getMessage());
+            }
+            hasData = true;
+        }
     }
-    
+
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
+        testsCompleted++;
+
+        if (testsCompleted == numberOfTests) {
+            Exception ErrM = null;
+            Connection connect = null;
+            PreparedStatement preparedStatementSpaceship = null;
+            PreparedStatement preparedStatementUser = null;
+            try {
+                System.out.println(String.format("---[DATABASE] %s", ConnectionString.getConnectionString()));
+                System.out.println("---[DATABASE] Setting up connection...");
+                Class.forName("com.mysql.jdbc.Driver");
+                connect = DriverManager.getConnection(ConnectionString.getConnectionString());
+                System.out.println("---[DATABASE] Connection established");
+                Integer spaceshipid;
+                preparedStatementUser = connect.prepareStatement("DELETE FROM account WHERE spaceship_id = ?;");
+                preparedStatementSpaceship = connect.prepareStatement("DELETE FROM spaceship WHERE id = ?;");
+
+                for (int i = 0; i < amountOfClients; i++) {
+                    spaceshipid = USERIDBASE + i;
+
+                    preparedStatementUser.setInt(1, spaceshipid);
+                    preparedStatementSpaceship.setInt(1, spaceshipid);
+
+                    preparedStatementUser.executeUpdate();
+                    preparedStatementSpaceship.executeUpdate();
+                    userList.remove(spaceshipid);
+                }
+            } catch (ClassNotFoundException | SQLException e) {
+                ErrM = e;
+            } finally {
+                preparedStatementUser.close();
+                preparedStatementSpaceship.close();
+                connect.close();
+            }
+            //check if an error was thrown while removing the users
+            assertNull(ErrM);
+            assertTrue(userList.isEmpty());
+            assertEquals(amountOfClients, amountLoggedIn);
+            
+            System.out.println("---Users removed---");
+            if (ErrM != null) {
+                System.out.println("Errors occurred: " + ErrM.getMessage());
+            }
+        }
     }
-    
+
     /**
-     * Test how many of the TestClient are initialized
-     * Also start the clients for the next test
+     * Test how many of the TestClient are initialised. Also start the clients
+     * for the next test
+     *
+     * @return Returns thrown errors.
      */
-    @Test
-    public void initializeTestClient(){
+    public Exception initializeTestClient() {
+        Exception ErrM = null;
         for (int i = 0; i < amountOfClients; i++) {
             boolean initiate = false;
             try {
-                TestClientRunnable r = new TestClientRunnable();
+                TestClientRunnable r = new TestClientRunnable(userList.get(USERIDBASE + i), IP_ADDRESS);
                 initiate = r.initiate();
                 executor.execute(r);
             } catch (SocketException ex) {
                 System.out.println(String.format("---[STRESSTEST] SocketException %s", ex.getMessage()));
+                ErrM = ex;
+            } catch (Exception ex) {
+                System.out.println(String.format("---[STRESSTEST] Exception %s", ex.getMessage()));
+                ErrM = ex;
             }
-            assertEquals(true, initiate);
+            if (initiate) {
+                amountLoggedIn++;
+            }
+            if (ErrM != null) {
+                ErrM = new Exception("Initiation failed.");
+            }
         }
+        return ErrM;
     }
-    
+
+    /**
+     * Shutdown all the clients
+     *
+     * @return Returns thrown errors.
+     */
+    public Exception shutDownTestClient() {
+        System.out.println("---[STRESSTEST] Shutting down");
+        Exception ErrM = null;
+        try {
+            executor.shutdown();
+
+            while (!executor.isTerminated()) {
+                //wait
+                //System.out.println("Waiting...");
+            }
+        } catch (Exception ex) {
+            ErrM = ex;
+        }
+        System.out.println("---[STRESSTEST] Shutted Down");
+        return ErrM;
+    }
+
     /**
      *
      */
     @Test
-    public void testClient(){
-        guiFrame.setVisible(true);
-        System.out.println("---[STRESSTEST] Start testClient");
-        int i = 0;
-        while(true){
-            if(running){
-                 int j = 1;
-                 i += j;
-            } else {
-                break;
-            }
+    public synchronized void testClient() {
+        Exception ErrM;
+
+        ErrM = initializeTestClient();
+        assertNull(ErrM);
+
+        try {
+            System.out.println("---[STRESSTEST] Start testClient");
+            wait(WAITTIME);
+//            int i = 0;
+//            while (true) {
+//                if (running) {
+//                    int j = 1;
+//                    i += j;
+//                } else {
+//                    break;
+//                }
+//            }
+//            System.out.println(i);
+
+//            String input = "";
+//            boolean isRunning = true;
+//            while (isRunning) {
+//                Scanner sc = new Scanner(System.in);
+//                input = sc.next();
+//                switch (input) {
+//                    case "e":
+//                    case "exit":
+//                        System.out.println("---[CONSOLE] Exit runtime...");
+//                        isRunning = false;
+//                        break;
+//                    default:
+//                        break;
+//                }
+//            }
+            System.out.println("---[STRESSTEST] Stop testClient");
+        } catch (Exception e) {
+            ErrM = e;
         }
-        System.out.println(i);
-        
-        System.out.println("---[STRESSTEST] Stop testClient");
-    }
-    
-    /**
-     * Shutdown all the clients
-     */
-    @Test
-    public void shutDownTestClient(){
-        System.out.println("---[STRESSTEST] Shutting down");
-        executor.shutdown();
-        
-        while(!executor.isTerminated()){
-        }
-        guiFrame.dispose();
-        System.out.println("---[STRESSTEST] Shutted Down");
+        assertNull(ErrM);
+
+        ErrM = shutDownTestClient();
+        assertNull(ErrM);
     }
 
     // TODO add test methods here. The name must begin with 'test'. For example:
--- src/test/java/com/ptssei/groepb/stresstest/TestClientRunnable.java
+++ src/test/java/com/ptssei/groepb/stresstest/TestClientRunnable.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.ptssei.groepb.stresstest;
 
+import com.ptsesd.groepb.shared.LoginComm;
+import com.ptsesd.groepb.shared.Serializer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -19,24 +15,25 @@ import java.rmi.UnknownHostException;
  * @author user
  */
 public class TestClientRunnable implements Runnable {
-    
+
     private static Socket socket;
     private static PrintWriter writer;
     private static BufferedReader reader;
-    
-    public static final String IP_ADDRESS = "192.168.24.78‏";
-    
-    public TestClientRunnable(){
-        
+    private final String username;
+    private final String IP_ADDRESS;
+
+    public TestClientRunnable(String username, String ip) {
+        this.username = username;
+        this.IP_ADDRESS = ip;
+        //System.out.println(username);
     }
-    
+
     public boolean initiate() throws SocketException {
         try {
             System.out.println("-----Initializing Comm Link to Server");
             socket = new Socket(IP_ADDRESS, 1337);
             System.out.println("Connection successful");
-            //socket = new Socket("localhost", 1337);
-            writer = new PrintWriter(socket.getOutputStream(),true);
+            writer = new PrintWriter(socket.getOutputStream(), true);
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             return true;
         } catch (UnknownHostException e) {
@@ -51,5 +48,13 @@ public class TestClientRunnable implements Runnable {
     @Override
     public void run() {
         //do work
+        System.out.println("Trying to login user: " + username);
+        //login first
+//        Runnable receiver = new LoginListener();
+//        Thread t = new Thread(receiver);
+//        t.start();
+        LoginComm lc = new LoginComm(LoginComm.class.getSimpleName(), username, username);
+        String json = Serializer.serializeLogin(lc);
+        writer.println(json);
     }
 }
