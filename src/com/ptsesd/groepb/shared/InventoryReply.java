b0dd6fd2eb8b2b8136c95d3732825a81d08d8378
sgijsber <bolletje94@gmail.com>
2014-04-24 13:59:57 +0200

    Added inventory request reply objects

---
+++ src/com/ptsesd/groepb/shared/InventoryReply.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ptsesd.groepb.shared;
+
+import java.util.List;
+
+/**
+ *
+ * @author user
+ */
+public class InventoryReply extends GamePacket{
+    
+    private final List<ItemComm> items;
+    private final int spaceshipId;
+    
+    public InventoryReply(String header, int spaceshipId, List<ItemComm> items){
+        super(header);
+        this.spaceshipId = spaceshipId;
+        this.items = items;
+    }
+
+    public int getSpaceshipId() {
+        return spaceshipId;
+    }
+
+    public List<ItemComm> getItems() {
+        return items;
+    }
+}
---
+++ src/com/ptsesd/groepb/shared/InventoryRequest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ptsesd.groepb.shared;
+
+/**
+ *
+ * @author user
+ */
+public class InventoryRequest extends GamePacket {
+    
+    private final int spaceshipId;
+    
+    public InventoryRequest(String header, int spaceshipId){
+        super(header);
+        this.spaceshipId = spaceshipId;
+    }
+
+    public int getSpaceshipId() {
+        return spaceshipId;
+    }
+}
