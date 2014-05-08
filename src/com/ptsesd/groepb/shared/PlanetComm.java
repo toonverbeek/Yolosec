d62276f1cbfb4b23d059d2ac1c560c62f655e8e3
Tim Kranen <tim.kranen@skindustries.net>
2014-05-08 14:50:44 +0200

    Added PlanetComm

---
+++ src/com/ptsesd/groepb/shared/PlanetComm.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ptsesd.groepb.shared;
+
+/**
+ *
+ * @author Tim
+ */
+public class PlanetComm extends GamePacket {
+    private String planetName;
+    private int size;
+    private float x, y;
+
+    public PlanetComm(String header, String planetName, int size, float x, float y) {
+        super(header);
+        this.planetName = planetName;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getPlanetName() {
+        return planetName;
+    }
+
+    public void setPlanetName(String planetName) {
+        this.planetName = planetName;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+    
+    
+    
+}
