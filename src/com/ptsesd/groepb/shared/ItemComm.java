b467b8eddfa3c74e2a0d7c997bf2260806684d41
KILL-MementoMori <p.f.sanderse@student.fontys.nl>
2014-05-09 11:45:39 +0200

    added planetdao

--- src/com/ptsesd/groepb/shared/ItemComm.java
+++ src/com/ptsesd/groepb/shared/ItemComm.java
@@ -9,17 +9,19 @@ public class ItemComm {
     private final Long itemId;
     private final int sellerId;
     private final AuctionHouseRequestType requestType;
-    private final int resourceAmount; 
+    private final int resourceAmount;
     private final String resourceType;
     private final String name;
+    private final String imageName;
 
-    public ItemComm(Long itemId, int sellerId, String name, int resourceAmount, String resourceType, AuctionHouseRequestType requestType) {
+    public ItemComm(Long itemId, int sellerId, String name, int resourceAmount, String resourceType, AuctionHouseRequestType requestType, String imageName) {
         this.itemId = itemId;
         this.sellerId = sellerId;
         this.name = name;
         this.requestType = requestType;
         this.resourceAmount = resourceAmount;
         this.resourceType = resourceType;
+        this.imageName = imageName;
     }
 
     public AuctionHouseRequestType getRequestType() {
@@ -44,5 +46,9 @@ public class ItemComm {
 
     public String getName() {
         return this.name;
+    }
+
+    public String getImageName() {
+        return this.imageName;
     }
 }
